62cb02c931761bb3c62687f6e3b1f012151edb4b
ThomasBackers <thomasbackers@gmail.com>
2021-12-06 12:28:56 +0100

    little victory \o/

--- src/index.php
+++ src/index.php
@@ -1,16 +1,9 @@
 <?php
 require 'db-config.php';
 
-try {
-  $pdo = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD, $OPTIONS);
-  echo 'Successfully connected!';
-
-  $query = $pdo->prepare('SELECT * FROM hikes');
-  $query->execute();
-  $results = $query->fetchAll(PDO::FETCH_ASSOC);
-} catch(PDOException $pdo_exception) {
-  echo 'ERROR: '.$pdo_exception->getMessage();
-}
+$query = $pdo->prepare('SELECT * FROM hikes');
+$query->execute();
+$results = $query->fetchAll(PDO::FETCH_ASSOC);
 ?>
 <pre>
   <?php print_r($results); ?>
