394ecbd21af9e75ae257c878354cf46c846a5275
Maxim K <90226452+coreinside5@users.noreply.github.com>
2021-12-08 15:53:56 +0100

    ???

--- src/index.php
+++ src/index.php
@@ -14,7 +14,6 @@ try {
 // PDO::FETCH_ASSOC to display only the columns as keys in the array returned
 $hikes = $q->fetchAll(PDO::FETCH_ASSOC);
 
-
 ?>
 
 <!DOCTYPE html>
@@ -40,7 +39,7 @@ $hikes = $q->fetchAll(PDO::FETCH_ASSOC);
             <div class="grid-item"><?php echo $hike["duration"]; ?></div>
             <div class="grid-item"><?php echo $hike["elevationGain"]; ?></div>
             <div class="grid-item">
-                <form method="post" action="delete.php?id=<?php echo $hike["ID"]; ?>"><input type="submit" name="delete-record" value="Delete record"></input></form>
+                <form method="post" action="delete.php?id=<?php echo $hike["ID"]; ?>" onsubmit="return confirm('Really Delete?');"><input type="submit" name="delete-record" value="Delete record"></input></form>
             </div>
             <div class="grid-item">
                 <form method="post" action="update.php?id=<?php echo $hike["ID"]; ?>"><input type="submit" name="update-record" value="Update record"></input></form>
@@ -50,10 +49,10 @@ $hikes = $q->fetchAll(PDO::FETCH_ASSOC);
                     <input type="submit" value="Create new" />
                 </form>
             </div>
-        </div>        
+        </div>
     <?php endforeach; ?>
-    
-   
+
+
 </body>
 
 </html>
