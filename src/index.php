4232bdb22e77fb3193109c96412142aeb9c0f0bd
Maxim K <90226452+coreinside5@users.noreply.github.com>
2021-12-10 10:16:24 +0100

    created/moidified field add

--- src/index.php
+++ src/index.php
@@ -4,7 +4,7 @@ require_once("connection.php");
 
 try {
     //this returns a PDOStatement object
-    $q = $pdo->prepare("SELECT ID,name, difficulty, distance, duration, elevationGain FROM hikes");
+    $q = $pdo->prepare("SELECT ID,name, difficulty, distance, duration, elevationGain, created_at, modified_at FROM hikes");
     //To execute the query set into $q (PDOStatement) object
     $q->execute();
 } catch (Exception $e) {
@@ -41,9 +41,18 @@ $hikes = $q->fetchAll(PDO::FETCH_ASSOC);
             <div class="grid-item"><?php echo $hike["distance"]; ?></div>
             <div class="grid-item"><?php echo $hike["duration"]; ?></div>
             <div class="grid-item"><?php echo $hike["elevationGain"]; ?></div>
+            <?php if ($hike["modified_at"] == null) : ?>
+
+                <div class = "grid-item" > Created at <?php echo $hike["created_at"]; ?> </div>
+
+            <?php else : ?>
+
+                <div class = "grid-item" > Modified at <?php echo $hike["modified_at"]; ?> </div>
+
+            <?php endif ?>
             <div class="grid-item">
                 <form class="delete-form" method="post" action="delete.php?id=<?php echo $hike["ID"]; ?>">
-                  <input type="submit" name="delete-record" value="Delete record"></input>
+                    <input type="submit" name="delete-record" value="Delete record"></input>
                 </form>
             </div>
             <div class="grid-item">
@@ -54,48 +63,47 @@ $hikes = $q->fetchAll(PDO::FETCH_ASSOC);
                     <input type="submit" value="Create new" />
                 </form>
             </div>
-        </div>        
+        </div>
     <?php endforeach; ?>
     <script>
-      $('.delete-form').on('submit', function(e){
-        e.preventDefault();
-        const swalWithBootstrapButtons = Swal.mixin({
-          customClass: {
-            confirmButton: 'btn btn-success',
-            cancelButton: 'btn btn-danger'
-          },
-          buttonsStyling: false
-        })
+        $('.delete-form').on('submit', function(e) {
+            e.preventDefault();
+            const swalWithBootstrapButtons = Swal.mixin({
+                customClass: {
+                    confirmButton: 'btn btn-success',
+                    cancelButton: 'btn btn-danger'
+                },
+                buttonsStyling: false
+            })
 
-        swalWithBootstrapButtons.fire({
-          title: 'Are you sure?',
-          text: "You won't be able to revert this!",
-          icon: 'warning',
-          showCancelButton: true,
-          confirmButtonText: 'Yes, delete it!',
-          cancelButtonText: 'No, cancel!',
-          reverseButtons: true
-        }).then((result) => {
-          if (result.isConfirmed) {
-            swalWithBootstrapButtons.fire(
-              'Deleted!',
-              'Your record has been deleted.',
-              'success'
-            )
-            e.target.submit()
-          } else if (
-            /* Read more about handling dismissals below */
-            result.dismiss === Swal.DismissReason.cancel
-          ) {
-            swalWithBootstrapButtons.fire(
-              'Cancelled',
-              'Database is safe :)',
-              'error'
-            )
-          }
-        })
-      })
+            swalWithBootstrapButtons.fire({
+                title: 'Are you sure?',
+                text: "You won't be able to revert this!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonText: 'Yes, delete it!',
+                cancelButtonText: 'No, cancel!',
+                reverseButtons: true
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    swalWithBootstrapButtons.fire(
+                        'Deleted!',
+                        'Your record has been deleted.',
+                        'success'
+                    )
+                    e.target.submit()
+                } else if (
+                    /* Read more about handling dismissals below */
+                    result.dismiss === Swal.DismissReason.cancel
+                ) {
+                    swalWithBootstrapButtons.fire(
+                        'Cancelled',
+                        'Database is safe :)',
+                        'error'
+                    )
+                }
+            })
+        })        
     </script>
 </body>
-
 </html>
