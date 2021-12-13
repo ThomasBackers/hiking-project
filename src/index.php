b361ce125a0c3d2f9dd0e61d6664305f1ddc0a90
Maxim K <90226452+coreinside5@users.noreply.github.com>
2021-12-13 11:16:33 +0100

    jscript extrenalized

--- src/index.php
+++ src/index.php
@@ -68,45 +68,6 @@ $hikes = $q->fetchAll(PDO::FETCH_ASSOC);
             </div>
         </div>
     <?php endforeach; ?>
-    <script>
-        $('.delete-form').on('submit', function(e) {
-            e.preventDefault();
-            const swalWithBootstrapButtons = Swal.mixin({
-                customClass: {
-                    confirmButton: 'btn btn-success',
-                    cancelButton: 'btn btn-danger'
-                },
-                buttonsStyling: false
-            })
-
-            swalWithBootstrapButtons.fire({
-                title: 'Are you sure?',
-                text: "You won't be able to revert this!",
-                icon: 'warning',
-                showCancelButton: true,
-                confirmButtonText: 'Yes, delete it!',
-                cancelButtonText: 'No, cancel!',
-                reverseButtons: true
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    swalWithBootstrapButtons.fire(
-                        'Deleted!',
-                        'Your record has been deleted.',
-                        'success'
-                    )
-                    e.target.submit()
-                } else if (
-                    /* Read more about handling dismissals below */
-                    result.dismiss === Swal.DismissReason.cancel
-                ) {
-                    swalWithBootstrapButtons.fire(
-                        'Cancelled',
-                        'Database is safe :)',
-                        'error'
-                    )
-                }
-            })
-        })        
-    </script>
+    <script type="text/javascript" src="scripts.js"></script>  
 </body>
 </html>
