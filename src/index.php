7b2a2c8916b15b1a24e6026f9736f315a7927ca2
Maxim K <90226452+coreinside5@users.noreply.github.com>
2021-12-14 11:12:25 +0100

    xxxx

--- src/index.php
+++ src/index.php
@@ -26,8 +26,7 @@ $hikes = $q->fetchAll(PDO::FETCH_ASSOC);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hikes</title>
-    <link rel="stylesheet" href="styles.css">
+    <title>Hikes</title>    
     <link href="./styles/main.min.css" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
